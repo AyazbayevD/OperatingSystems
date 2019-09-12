5cfddc90dcffe4a9bd22a0ab37acee529f22bdc0
AyazbayevD <42997788+AyazbayevD@users.noreply.github.com>
2019-09-12 20:55:45 +0300

    Update ex1.c

--- week04/ex1.c
+++ week04/ex1.c
@@ -9,6 +9,6 @@ int main(){
 	printf("Hello from child [PID-%d]\n\n", n);
 	return 0;
 }
-//program will output hellow from parent and child 20 times,
-//because for each program run hello from parent anad child
+//program will output hello from parent and child 20 times,
+//because for each program run hello from parent and child
 //will be output 2 times because of two processes for each program run
