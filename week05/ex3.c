598d7936d3b6a2edcc2683f05f301925bd08bac4
AyazbayevD <42997788+AyazbayevD@users.noreply.github.com>
2019-09-20 00:02:32 +0300

    Add files via upload

--- week05/ex3.c
+++ week05/ex3.c
@@ -4,21 +4,42 @@
 #include <pthread.h>
 #define ll long long
 
-const ll MXN = 1e9 + 1;
+const ll MXN = 1e4 + 1;
 
 //we are simulating buffer
 //if we will create real buffer like array, there can be problems 
 //with accessing non-existing parts of memory
 ll buff_sz; 
+int consumer_sleep, producer_sleep;
+
+void awake_consumer(){
+	consumer_sleep = 0;
+}
+
+void sleep_consumer(){
+	consumer_sleep = 1;
+}
+
+void awake_producer(){
+	producer_sleep = 0;
+}
+
+void sleep_producer(){
+	producer_sleep = 1;
+}
 
 void *produce (){
 	while (1){
+		if (buff_sz > 0){
+			awake_consumer();
+		}
 		if (buff_sz < MXN){
 			buff_sz++;
 		}
 		else {
+			sleep_producer();
 			while(1){
-				if (buff_sz < MXN){
+				if (producer_sleep == 0){
 					break;
 				}
 			}
@@ -27,12 +48,16 @@ void *produce (){
 }
 void *consume (){
 	while (1){
+		if (buff_sz < MXN){
+			awake_producer();
+		}
 		if (buff_sz > 0){
 			buff_sz--;
 		}
 		else {
+			sleep_consumer();
 			while (1){
-				if (buff_sz > 0){
+				if (consumer_sleep == 0){
 					break;
 				}
 			}
